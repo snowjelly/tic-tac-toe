1b74fe0a8793bafc90e83d9446c6508eb94cbc4c
snowjelly <broadwellconnor@gmail.com>
2022-09-20 14:33:44 -0500

    changes

--- script.js
+++ script.js
@@ -24,25 +24,45 @@ const DisplayController = (() => {
 })();
 
 const Player = (name) => {
-  const player = {
-    name,
-    turn: false,
-    input: ''
-  }
+    const player = {
+      name,
+      turn: false,
+      input: ''
+    }
 
-  return {
-    player
-  };
+    const get = () => {
+      return player;
+    }
+
+    const updateTurn = () => {
+      if (player.turn === true) {
+        player.turn = false;
+      }
+      else if (player.turn === false){
+        player.turn = true;
+      }
+    }
+  return {get, updateTurn};
 }
 
 
 const TicTacToe = (() => {
-  const player1 = Player('snowinjelly');
-  const player2 = Player('bot');
+  const player1 = Player('snow');
+  const player2 = Player('jelly');
 
-  //while (GameBoard.get().length < 9) {
-    player1.turn = true;
-    player1.input = prompt('Player 1: X or O');
-    GameBoard.get().push(player1.input);
-  return {player1, player2}
+  const getPlayers = () => {
+    return [player1.get(), player2.get()];
+  }
+  
+  const nextTurn = () => {
+    player1.updateTurn();
+    player2.updateTurn();
+  }
+
+
+  const start = () => {
+    //while (GameBoard.get().length < 9) {
+    player1.get().turn = true;
+  }
+  return {getPlayers, start, nextTurn};
 })();
