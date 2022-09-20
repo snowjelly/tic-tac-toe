0654a6cd22130928800c1e763634bfec13a4facc
snowjelly <broadwellconnor@gmail.com>
2022-09-19 20:51:42 -0500

    added tictactoe module and more skeleton idea

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const gameBoard = (() => {
+const GameBoard = (() => {
   const gameBoard = [];
+  const boardSignature = [];
 
+  const get = () => {
+    return gameBoard;
+  }
 
-  const getGameBoard = () => {
-    
-  };
+  const getSignature = () => {
+    return boardSignature;
+  }
+
+  return {get, getSignature};
 })();
 
-const displayController = (() => {
+const DisplayController = (() => {
   const getDisplay = () => {
 
   };
@@ -18,6 +24,25 @@ const displayController = (() => {
 })();
 
 const Player = (name) => {
+  const player = {
+    name,
+    turn: false,
+    input: ''
+  }
 
+  return {
+    player
+  };
 }
 
+
+const TicTacToe = (() => {
+  const player1 = Player('snowinjelly');
+  const player2 = Player('bot');
+
+  //while (GameBoard.get().length < 9) {
+    player1.turn = true;
+    player1.input = prompt('Player 1: X or O');
+    GameBoard.get().push(player1.input);
+  return {player1, player2}
+})();
