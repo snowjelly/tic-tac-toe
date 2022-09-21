3742297072dc0932d8d187ed4dc96f1b118c28ca
snowjelly <broadwellconnor@gmail.com>
2022-09-21 15:45:01 -0500

    reorganized to fix lexical error

--- script.js
+++ script.js
@@ -24,52 +24,38 @@ const GameBoard = (() => {
   return {getBoard, check, length};
 })();
 
-const DisplayController = (() => {
-  const board = document.querySelector('.board');
-  const render = () => {
-    for (i=0;i<9;i++) {
-      const btn = document.createElement('button');
-      btn.setAttribute('data-position', i.toString());
-      board.appendChild(btn);
-    }
-    
+const Player = (name, input) => {
+  const player = {
+    name,
+    turn: false,
+    input
   }
 
-  return {render};
-})();
+  const get = () => {
+    return player;
+  }
 
-const Player = (name, input) => {
-    const player = {
-      name,
-      turn: false,
-      input
+  const updateTurn = () => {
+    if (player.turn === true) {
+      player.turn = false;
     }
-
-    const get = () => {
-      return player;
+    else if (player.turn === false){
+      player.turn = true;
     }
+  }
 
-    const updateTurn = () => {
-      if (player.turn === true) {
-        player.turn = false;
-      }
-      else if (player.turn === false){
-        player.turn = true;
-      }
-    }
-
-    const play = () => {
-      if (player.turn && GameBoard.length() < 9) {
-        GameBoard.getBoard().push(player.input);
-        GameBoard.getSignature().push(player.name);
-        TicTacToe.nextTurn();
-        if (GameBoard.length() >= 3) {
-          GameBoard.check();
-        }
+  const play = () => {
+    if (player.turn && GameBoard.length() < 9) {
+      GameBoard.getBoard().push(player.input);
+      GameBoard.getSignature().push(player.name);
+      TicTacToe.nextTurn();
+      if (GameBoard.length() >= 3) {
+        GameBoard.check();
       }
     }
+  }
 
-  return {get, updateTurn, play};
+return {get, updateTurn, play};
 }
 
 const TicTacToe = (() => {
@@ -78,6 +64,14 @@ const TicTacToe = (() => {
 
   const getPlayers = () => {
     return [player1.get(), player2.get()];
+  }
+
+  const whosTurn = () => {
+    for (i=0;i<getPlayers().length;i++) {
+      if (getPlayers()[i].turn = true) {
+        return getPlayers()[i].name;
+      }
+    }
   }
   
   const nextTurn = () => {
@@ -89,9 +83,29 @@ const TicTacToe = (() => {
   const start = () => {
     //while (GameBoard.get().length < 9) {
     player1.get().turn = true;
-    player1.play();
   }
-  return {getPlayers, start, nextTurn};
+  return {getPlayers, start, nextTurn, whosTurn};
 })();
+
+const DisplayController = (() => {
+  const board = document.querySelector('.board');
+  const turnInfo = document.querySelector('.info h2');
+  board.addEventListener('click', (event) => {
+    console.log(event.target);
+    turnInfo.textContent = TicTacToe.whosTurn();
+  });
+  const render = () => {
+    for (i=0;i<9;i++) {
+      const btn = document.createElement('button');
+      btn.setAttribute('data-position', i.toString());
+      board.appendChild(btn);
+    }
+    turnInfo.textContent = TicTacToe.whosTurn();
+  }
+
+  return {render};
+})();
+
+
 
 DisplayController.render();
