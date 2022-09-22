cb3c673dbe2fa3315394ff5d42c04185ad3d5f8c
snowjelly <broadwellconnor@gmail.com>
2022-09-22 11:15:55 -0500

    basic checkWin logic

--- script.js
+++ script.js
@@ -8,8 +8,48 @@ const GameBoard = (() => {
   }
 
   const checkWin = () => {
-    if (board[0] === board[1] && board[1] === board[2] && board[0] !== '') {
-      return true
+    let startPos = 0;
+    /* ['X', 'X', 'X',
+        '', '', '',
+        '', '', ''];
+    */
+    if (board[startPos] === board[startPos + 1] && board[startPos + 1] === board[startPos + 2] && board[startPos] !== '') {
+      return true;
+    }
+    /* ['', '', '',
+        'X', 'X', 'X',
+        '', '', ''];
+    */
+    if (board[3] === board[4] && board[4] === board[5] && board[3] !== '') {
+      return true;
+    }
+    /* ['', '', '',
+        '', '', '',
+        'X', 'X', 'X'];
+    */
+    if (board[6] === board[7] && board[7] === board[8] && board[6] !== '') {
+      return true;
+    }
+    /* ['X', '', '',
+        'X', '', '',
+        'X', '', ''];
+    */
+    if (board[0] === board[3] && board[3] === board[6] && board[0] !== '') {
+      return true;
+    }
+    /* ['', 'X', '',
+        '', 'X', '',
+        '', 'X', ''];
+    */
+    if (board[1] === board[4] && board[4] === board[7] && board[1] !== '') {
+      return true;
+    }
+    /* ['', '', 'X',
+        '', '', 'X',
+        '', '', 'X'];
+    */
+    if (board[2] === board[5] && board[5] === board[8] && board[2] !== '') {
+      return true;
     }
   }
 
